02ca9a001a0e96917c03d5c53d4bf1442ccdcb6c
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-03 21:58:29 -0500

    added messages component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -6,6 +6,7 @@ import './Chat.css'
 
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
@@ -56,6 +57,7 @@ const Chat = ({ location })=> { //location is a prop coming from react router
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
