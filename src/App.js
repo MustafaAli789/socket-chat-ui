982e38b406d0df46283ff21be8b78b294878e5e9
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-03 19:18:28 -0500

    join component created with link to chat room

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Join from './components/Join';
-import Join from './components/Chat';
+import Chat from './components/Chat';
 
 const App = ()=>(
     <Router>
