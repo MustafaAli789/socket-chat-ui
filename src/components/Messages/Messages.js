b4c8565f0d42a7dbc8e5f0dd44bd6efb64d9cb1f
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-04 21:09:06 -0500

    message component added and styled

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import './Messages.css';
 
+import Message from '../Message/Message';
+
 const Messages = ({ messages, name }) => {
     return (
         <ScrollToBottom>
